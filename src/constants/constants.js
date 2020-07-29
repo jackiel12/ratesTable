075cf9ee431f861fa4629239baf7a002a641a767
Jackie Lin <jackie.lin128@gmail.com>
2020-07-29 09:10:11 -0400

    moved a few variables to a constants folder for modularity and better organization

---
+++ src/constants/constants.js
@@ -0,0 +1,6 @@
+const Constants = {
+    CoinCodes: ["BTC", "ETH", "DAI", "USDC", "USDT", "WBTC"],
+    RatesAPI: "https://liquality.io/swap/agent/api/swap/marketinfo"
+}
+
+module.exports = Constants
